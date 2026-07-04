2dc8f135735529c596cf9fcc8e865f7fec89efd3
Aster42 <ri04.raj42@gmail.com>
2026-07-04 12:56:05 +0000

    Add the refactored version to merge with main

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from mftool import Mftool
 import csv
 
@@ -16,20 +15,17 @@ with open("Data/Directory.csv", "r", newline='') as d:
 field_names = [
     "fund_house","scheme_type","scheme_category",
     "scheme_code","scheme_name","scheme_start_date",
-    "52_week_high","52_week_low"]
+    "52_week_high","52_week_low"
+]
 
 nav_field = ['date', 'nav']
 
-for key in keys[8800:]:
-    raw = mf.get_scheme_historical_nav(key)
-    if raw == None:
-        continue
-    with open("Data/Fund_Data.csv", "a", newline='') as fund:
-        writer = csv.DictWriter(fund, fieldnames=field_names, extrasaction='ignore')
+with open("Data/Fund_Data.csv", "a", newline='') as fund:
+    writer = csv.DictWriter(fund, fieldnames=field_names, extrasaction='ignore')
+    for key in keys[11190:]:
+        raw = mf.get_scheme_historical_nav(key)
+        if raw == None:
+            continue
+
         writer.writerow({field: raw[field] for field in field_names})
 
-    # with open(f"Data/{raw["fund_house"]}.csv", "w", newline='') as nav:
-    #     writer = csv.DictWriter(nav, fieldnames=nav_field)
-    #     writer.writeheader()
-    #     for entry in raw['data']:
-    #         writer.writerow({fields: entry[fields] for fields in nav_field})
