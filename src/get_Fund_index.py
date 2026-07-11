62b2a23eff9271eab17c5f663b2f48127dd78a81
Aster42-03 <ri04.raj42@gmail.com>
2026-07-11 19:51:55 +0530

    feat: Add clear progress bar

--- src/get_Fund_index.py
+++ src/get_Fund_index.py
@@ -1,10 +1,14 @@
-from concurrent.futures import ThreadPoolExecutor
+from tqdm.contrib.concurrent import thread_map
+import psycopg2.extras, psycopg2.pool
 from dotenv import load_dotenv
 from datetime import datetime
 from mftool import Mftool
 import psycopg2
+import random
+import json
 import time
 import os
+import re
 
 # --- Initialization ---
 mf = Mftool()
@@ -20,20 +24,23 @@ DB_CONFIG = {
 
 keys = sorted(list(mf.get_scheme_codes().keys()))
 
+conn_pool = psycopg2.pool.ThreadedConnectionPool(minconn=5, maxconn=20, **DB_CONFIG)
 
+# ---------------------------------------------------------------------------------------------
 def test_connection():
     """
     Tests the connection to the database for any given connection variable
     :return: connection
     """
     try:
-        connection = psycopg2.connect(**DB_CONFIG)
+        connection =  conn_pool.getconn()
         return connection
 
     except psycopg2.OperationalError as e:
         print(f"Connection failed. {e}")
         return None
 
+# ---------------------------------------------------------------------------------------------
 
 # --- Creating DataBase Tables ---
 conn = test_connection()
@@ -67,13 +74,17 @@ conn.commit()
 
 # --- Load already completed keys ---
 cursor.execute("SELECT scheme_code FROM checkpoint_index")
-completed = {row[0] for row in cursor.fetchall()}
+completed = {str(row[0]) for row in cursor.fetchall()}
 remaining_keys = [k for k in keys if k not in completed]
 print(f"{len(remaining_keys)}: Scheme Codes Left to be Processed")
 
 cursor.close()
-conn.close()
+conn_pool.putconn(conn)
 
+# ---------------------------------------------------------------------------------------------
+
+MAX_RETRIES = 5
+BASE_DELAY = 0.5
 
 # --- Worker Function to process each fund and load in DataBase
 def process_fund(code):
@@ -83,38 +94,57 @@ def process_fund(code):
     :return: Success, code
     """
 
-    # managing api request limit
-    time.sleep(0.5)
-
-    # console message
-    print(f"Currently Processing Code: {code}")
-
     # worker connection to database
     con = test_connection()
     if con is None:
-        print(f"No Connection for Code: {code} - Skipping.")
-        return False, code
+        return False, code, f"No Connection for Code - Skipping."
     cursor = con.cursor()
+
+    attempts = 0
+    while attempts < MAX_RETRIES:
+        try:
+            time.sleep(random.uniform(0.2, 0.5))
+            details = mf.get_scheme_details(code)
+            break
+
+        except Exception as e:
+            attempts += 1
+            if attempts >= MAX_RETRIES:
+                return False, code, f"Maximum Retries Reached, Error: {e}"
+
+        time.sleep((BASE_DELAY * (2 ** attempts - 1)) + random.uniform(0.1, 0.5))
 
     # get raw data from the API
     try:
-        details = mf.get_scheme_details(code)
+
         # Validate API response
         if not details or "scheme_start_date" not in details:
-            print(f"Invalid API data for code: {code} - Skipping")
-            return False, code
+            return False, code, f"Invalid API data for code - Skipping"
+
         dates = details.pop("scheme_start_date")  # get the nested dict inside raw
+
+        try:
+            clean_date = datetime.strptime(dates.get("date"), "%d-%m-%Y").strftime("%Y-%m-%d")
+            clean_nav = float(dates.get("nav"))
+        except (ValueError, TypeError):
+            # If the API returns 'N/A' or None, insert NULLs into the database
+            clean_date = None
+            clean_nav = None
 
         # 2. Explicitly map variables to guarantee exact SQL order
         row = (
             details.get("fund_house"),
             details.get("scheme_type"),
             details.get("scheme_category"),
-            details.get("scheme_code"),
+            int(details.get("scheme_code")),
             details.get("scheme_name"),
-            dates.get("date"),
-            dates.get("nav"),
+            clean_date,
+            clean_nav,
         )
+
+        if not re.search(r"\w", str(details.get("fund_house", ""))):
+            return False, code, f"Invalid Entry"
+
 
         cursor.execute(
             """
@@ -124,31 +154,49 @@ def process_fund(code):
         """,
             row,
         )
-        print(f" -> Successfully Inserted Data for Code: {code}")
 
         # Update the Checkpoint
         # --- Update checkpoint after successful insertion ---
         checkpoint_query = """
-                           INSERT INTO checkpoint_index VALUES %s 
+                           INSERT INTO checkpoint_index (scheme_code) VALUES (%s) 
                            ON CONFLICT (scheme_code) DO NOTHING 
                            """
         cursor.execute(checkpoint_query, (code,))
         con.commit()
-        return True, code
+        return True, code, f"Inserted Data"
+
 
     except Exception as e:
-        print(f"Insertion error: {e}")
         con.rollback()
-        return False, code
+        return False, code, f"Insertion Error: {e}"
     finally:
         cursor.close()
-        con.close()
+        conn_pool.putconn(con)
 
+try:
+    results = thread_map(process_fund, remaining_keys, max_workers=20, desc='Logging Indices', unit='Fund')
 
-with ThreadPoolExecutor(max_workers=3) as executor:
-    result = executor.map(process_fund, remaining_keys)
     failed = 0
-    for success, k in result:
-        if not success:
-            failed += 1
-    print(f"Process Completed. {failed} Codes Failed, Checkpoint Kept for Resumption")
+    # Log the progress in a .jsonl file
+    with open("../Data/index_logs.jsonl", "w", newline="\n") as log:
+
+        for success, key, message in results:
+
+            log_record = {
+                "timestamp": datetime.now().isoformat(),
+                "scheme_code": key,
+                "status": "SUCCESS" if success else "FAILED",
+                "message": message,
+            }
+
+            log.write(json.dumps(log_record) + "\n")
+
+            if not success:
+                failed += 1
+
+except Exception as e:
+    print(f"Unknown Thread Crash: {e}")
+
+finally:
+    print(f"Process Completed. Checkpoint Kept for Resumption, Check Logs for Further Info.")
+    conn_pool.closeall()
