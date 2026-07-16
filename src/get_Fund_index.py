1548db5a47740d0ae2f58f9fde83deaf8a5bdb30
Aster42-03 <ri04.raj42@gmail.com>
2026-07-16 12:18:12 +0530

    chore: Remove unused pyscopg2.extras import

--- src/get_Fund_index.py
+++ src/get_Fund_index.py
@@ -8,7 +8,6 @@ from datetime import datetime
 from dotenv import load_dotenv
 from tqdm.contrib.concurrent import thread_map
 import psycopg2
-import psycopg2.extras
 import psycopg2.pool
 from mftool import Mftool
 
