4f1c2d8876006397c2b66fb6124930a7c69f3cbe
Aster42 <ri04.raj42@gmail.com>
2026-08-01 22:02:10 +0530

    feat: Add Test For Nav Endpoint of the API

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,35 +5,44 @@ from app.main import app
 
 
 # Creating Fake Browser
-@pytest.fixture(scope="module")
+@pytest.fixture( scope = "module" )
 def client():
-    with TestClient(app) as test_client:
+    with TestClient( app ) as test_client:
         yield test_client
 
 
 # Functions to Test The Root Endpoint
-def test_root_endpoint_returns_200(client):
-    response = client.get("/")
+def test_root_endpoint_returns_200( client ):
+    response = client.get( "/" )
 
     assert response.status_code == 200
-    assert response.json() == {"message": "Hello"}
+    assert response.json() == { "message": "Hello" }
 
 
-def test_get_fund(client):
-    response = client.get("/fund/100034")
+def test_get_fund( client ):
+    response = client.get( "/fund/108272" )
 
     assert response.status_code == 200
     assert response.json() == {
         "House": "Aditya Birla Sun Life Mutual Fund",
         "Type": "Open Ended Schemes",
-        "Category": "Equity Scheme - Large & Mid Cap Fund",
-        "Code": 100034,
-        "Name": "Aditya Birla Sun Life Large & Mid Cap Fund -Regular - IDCW",
-        "Start Date": "2006-04-03",
+        "Category": "Debt Scheme - Banking and PSU Fund",
+        "Code": 108272,
+        "Name": "Aditya Birla Sun Life Banking & PSU Debt Fund  - REGULAR - IDCW",
+        "Start Date": "2008-05-08"
     }
 
 
-def test_get_fund_not_found(client):
-    response = client.get("/fund/999999")
+def test_get_fund_not_found( client ):
+    response = client.get( "/fund/999999" )
+
     assert response.status_code == 404
-    assert response.json() == {"detail": "Fund not Found"}
+    assert response.json() == { "detail": "Fund not Found" }
+
+
+def test_get_nav( client ):
+    response = client.get( "/fund/nav/108272" )
+    data = response.json()
+
+    assert response.status_code == 200
+    assert isinstance( data, list )
